0866384b23d56a28a1aba8912cdbe6866b95af30
Valefernandez003 <141738312+Valefernandez003@users.noreply.github.com>
2023-08-16 21:44:52 -0300

    Add files via upload

---
+++ ejercicios_complementarios.py
@@ -0,0 +1,44 @@
+#ejercicio 1
+num1=12
+#ejercicio 2
+num2=3.5
+#ejercicio 3
+suma=num1+num2
+#ejercicio 4
+resta=num1-num2
+print(resta)
+division=num1/num2
+print(division)
+multiplicacion=num1*num2
+print(multiplicacion)
+#ejercicio 5
+nombre="Valentín"
+#ejercicio 6
+precio=1250
+#ejercicio 7
+descuento=0.25
+#ejercicio 8
+precio_final=precio-(precio*descuento)
+#ejercicio 9
+cadena="hola cómo estás?"
+#ejercicio 10
+longitud=len(cadena)
+#ejercicio 11
+precio=int(1250.33)
+#ejercicio 12
+nombre="Juan"
+apellido="Rodríguez"
+nombre_apellido=nombre+" "+apellido
+#ejercicio 13
+edad=19
+edad=edad+5
+edad=edad-10
+#ejercicio 14
+altura=1.80
+altura=altura*4
+altura=altura/3
+#ejercicio 15
+nombre_mayus="VALENTÍN FERNANDEZ"
+print(nombre_mayus.lower())
+#ejercicio 16
+print(nombre_mayus.title())
