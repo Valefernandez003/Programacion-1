48259463439594933b44ab0460c6ce608ed2bf17
Valefernandez003 <141738312+Valefernandez003@users.noreply.github.com>
2023-08-30 20:56:19 -0300

    Update ejercicio_condicionales.py

--- ejercicio_clase.py
+++ ejercicio_clase.py
@@ -1,50 +1,58 @@
 def main(): 
     try:
-        #pedimos al usuario que ingrese la fecha 
-        fecha_actual=str(input("Ingrese la fecha en el siguiente formato: Día,DD/MM: "))
-        dia_semana, fecha = fecha_actual.split(',') #con un split dividimos la variable fecha actual en dos variables, una para almacenar el dia y otra para almacenar la fecha
-        dia, mes = fecha.split('/') #divido la fecha en dos variables, cada una que va a almacenar una parte de la fecha
-        dia_semana=dia_semana.lower()
-        dia_semana_valido=["lunes","martes","miercoles","jueves","viernes"]
-
-        if dia_semana not in dia_semana_valido or int(dia) > 31 or int(mes) > 12:  #condiciones para que salga un mensaje de error
-         print("Error, un dato ingresado es incorrecto")
-         return
-        #segun caso con los dias de la semana
+        #pedimos al usuario que ingrese la fecha
+        fecha_actual = input("Ingrese la fecha en el siguiente formato: Día,DD/MM: ")
+        dia_semana, fecha = fecha_actual.split(',')
+        dia, mes = fecha.split('/')
+        dia_semana = dia_semana.lower()
+        dia_semana_valido = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+        #validamos el dia ingresado
+        if dia_semana not in dia_semana_valido or int(dia) > 31 or int(mes) > 12:
+            print("Error, un dato ingresado es incorrecto")
+            return
+        #switch case dias de la semana
         if dia_semana == "lunes":
-            examen = input("se tomo examen?")
+            print("nivel inicial")
+            examen = input("se tomó examen?")
             procesar_examen(examen)
-        elif dia_semana== "martes":
-             examen = input("se tomo examen?")
-           procesar_examen(examen)
-        elif dia_semana=="miercoles":
-         examen = input("se tomo examen?")
-           procesar_examen(examen)
-        elif dia_semana== "jueves":
-           practica_hablada()
-        elif dia_semana== "viernes":
-           ingles_viajero(dia,mes)  
+        elif dia_semana == "martes":
+                print("nivel intermedio")
+                examen = str(input("se tomó examen?"))
+                procesar_examen(examen)
+        elif dia_semana == "miercoles":
+                print("nivel avanzado")
+                examen = input("se tomó examen?")
+                procesar_examen(examen)
+        elif dia_semana == "jueves":
+                 practica_hablada()
+        elif dia_semana == "viernes":
+                 ingles_viajero(dia, mes)
     except ValueError:  
-        print("ocurrió un error")
-def procesar_examen(): #funcion que saque el porcentaje de alumnos aprobados
+        print("Ocurrió un error")
+        #funcion que calcule el porcentaje de alumnos aprobados
+def procesar_examen(examen):
     if examen == "si":
-    alumnos = int(input("ingrese la cantidad de alumnos del curso "))
-    aprobados=int(input("ingrese la cantidad de alumnos aprobados "))
-    porcentaje = (aprobados/alumnos)*100
-    print(f"el porcentaje de alumnos aprobados es: {porcentaje}%")
+        alumnos = int(input("Ingrese la cantidad de alumnos del curso: "))
+        aprobados = int(input("Ingrese la cantidad de alumnos aprobados: "))
+        porcentaje = (aprobados / alumnos) * 100
+        print(f"El porcentaje de alumnos aprobados es: {porcentaje}%")
     pass
-def practica_hablada(): #funcion que dice si fue mas de el 50% de la clase
-    asistencia= float(input("ingrese el porcentaje de asistencia sin '%': "))
+        #funcion que calcule la asistencia
+def practica_hablada():
+    asistencia = float(input("Ingrese el porcentaje de asistencia sin '%': "))
     if asistencia > 50:  
-        print("asistió la mayoría")
+        print("Asistió la mayoría")
     else:
-        print("faltó la mayoría")
-def ingles_viajero(dia,mes): #funcion que saca el total de los aranceles
-    arancel=float(input("ingrese el arancel por alumno: "))
-    alumnos= int(input("ingrese la cantidad de alumnos: "))
-    if (dia==1 and mes==1) or (dia==1 and mes==7):
-      print("comienzo de un nuevo ciclo")
-    pass
-    print("el ingreso total sera de: ", arancel*alumnos,"$")
+        print("Faltó la mayoría")
+        #funcion que calcule los aranceles
+def ingles_viajero(dia, mes):
+    arancel = float(input("Ingrese el arancel por alumno: "))
+    alumnos = int(input("Ingrese la cantidad de alumnos: "))
+    if (dia == 1 and mes == 1) or (dia == 1 and mes == 7):
+        print("Comienzo de un nuevo ciclo")
+    
+    print("El ingreso total será de:", arancel * alumnos, "$")
+
 if __name__ == "__main__":
-        main()
+    main()
+
